355271c96b1622f55f35ac0658a8d6354674fe79
Wasfun <halco.figueroa@gmail.com>
2020-10-16 17:30:11 -0500

    add credits integration

--- plugins/speak/speak.js
+++ plugins/speak/speak.js
@@ -2,7 +2,7 @@ const Bot = require('../../modules/Bot.js');
 const say = require('say')
 var path = require('path');
 const settings = require('./speak.json');
-
+const alerts = require('../alerts/alerts.js');
 
 module.exports = {
   name: 'speak',
@@ -13,12 +13,15 @@ module.exports = {
   permissions: ['moderator', 'creator'],
   cooldown: 10, // this is Set in Seconds, how long between the next usage of this command.
   execute(client, data) {
-
+    const userCredits = users_credits[data.user];
     if (data.args.length > 0) {
-      if(data.args[0] == 'stop'){
+      if (data.args[0] == 'stop') {
         say.stop();
       } else {
-        say.speak(data.args.join(' '), settings.voice, settings.speed)
+        if (!!userCredits && (userCredits.credits | 0) > 3) {
+          say.speak(data.args.join(' '), settings.voice, settings.speed);
+          users_credits[data.user].credits = userCredits.credits - 3;
+        }
       }
     }
     else {
